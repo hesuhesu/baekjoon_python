24a91596dccfc3c68afb8f583115ab81ee5eaad3
hesuhesu <zndwk0728@gmail.com>
2024-02-12 00:36:43 +0900

    add 5086 problem

---
+++ 5086(배수와 약수)/5086.py
@@ -0,0 +1,13 @@
+import sys
+
+while 1 :
+    A,B = map(int, sys.stdin.readline().split())
+    if A == 0 and B == 0 :
+        break
+    
+    if B % A == 0 :
+        print("factor")
+    elif A % B == 0 :
+        print("multiple")
+    else :
+        print("neither")
