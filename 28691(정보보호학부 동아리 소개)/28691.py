4ef74d8191d952be7733fee81272bebb321e4c2d
hesuhesu <91324571+hesuhesu@users.noreply.github.com>
2024-08-12 00:34:48 +0900

    Create 28691.py

---
+++ 28691(정보보호학부 동아리 소개)/28691.py
@@ -0,0 +1,7 @@
+import sys
+charA = sys.stdin.readline().strip()
+if (charA == 'M'): print('MatKor')
+elif (charA == 'W'): print('WiCys')
+elif (charA == 'C'): print('CyKor')
+elif (charA == 'A'): print('AlKor')
+elif (charA == '$'): print('$clear')
